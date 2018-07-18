7ae216e8811b3a1226fe5e0be7c6c3bda02e7f1d
Frederic Heem <frederic.heem@gmail.com>
2018-07-18 20:20:09 +0100

    ed25519 compiles

--- src/ed25519-donna/ed25519.cpp
+++ src/ed25519-donna/ed25519.cpp
@@ -6,9 +6,10 @@
 
 
 /* define ED25519_SUFFIX to have it appended to the end of each public function */
-
+#define ED25519_CUSTOMRANDOM
 #define ED25519_FORCE_32BIT
-#error 
+#define ED25519_REFHASH
+
 #if !defined(ED25519_SUFFIX)
 #define ED25519_SUFFIX 
 #endif
